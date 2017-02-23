85f717b80a3bd685d2ca8a1b736b2fd23b084ca9
Fatima Berazi <fatimaberaze1997@gmail.com>
2017-02-23 19:41:07 +0300

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,22 +1,18 @@
-function callMe() {
+function callMe(){
   var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  
 }
 
-
-function crazy() {
-  // fix the code in here:
-  
+function crazy(){
+  //fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
   thisIsCrazy();
-
 }
 
 function sayMyName() {
@@ -27,8 +23,6 @@ function sayMyName() {
 
   function sayMy() {
     var name = "Kristin";
-    console.log(name);
-    
-  }
 
-}
+    console.log(name);
+  }
