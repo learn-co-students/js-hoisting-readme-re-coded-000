f2d8f4a27f3fb54201632d8264355ee255151807
Ahmedalflahy <lfight522@gmail.com>
2017-02-24 19:45:55 +0300

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,19 @@
 function callMe() {
+	 var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
+callMe();
 
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
+function crazy(){
+  //fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -24,7 +23,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
+  	 var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+   
   }
 }
